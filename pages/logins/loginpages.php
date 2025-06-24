35378830e29196c40aea806c57db66cb0e0c72b4
Allen Jefferson Orcino <allenjeffersonorcino@gmail.com>
2025-06-24 13:56:30 +0800

    fist commit

---
+++ pages/logins/loginpages.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <rel="stylesheet" href="Styes/loginpage.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <!--Input code for login page --->
+</body>
+</html>
