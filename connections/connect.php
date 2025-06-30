de6b78f85d3dea96bc89d788726c95f0519f5f03
Allen Jefferson Orcino <allenjeffersonorcino@gmail.com>
2025-06-30 19:22:57 +0800

    add new connect.php and slight changes system banner

---
+++ connections/connect.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "";
+
+// to follow to database for classtrack pro 
+
+$conn=new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error){
+    echo "Connection failed " . $conn->connect_error;
+}
+
+
+?>
